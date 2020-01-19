e9a83c40a24f80fa8dd1817a6389fd0e8e070e3b
helderavila <helderavila2016@gmail.com>
2020-01-19 12:12:19 -0300

    notification of buy and router

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const vm = new Vue({
   data: {
     produtos: [],
     produto: false,
-    carrinho: []
+    carrinho: [],
+    mensagemAlerta: "Item adicionado ao carrinho",
+    alertaAtivo: false
   },
   filters: {
     numeroPreco(valor) {
@@ -52,6 +54,7 @@ const vm = new Vue({
       this.produto.estoque--
       const { id, nome, preco } = this.produto
       this.carrinho.push({ id, nome, preco })
+      this.alerta(`${nome} adicionado ao carrinho`)
     },
     removerItem(index) {
       this.carrinho.splice(index, 1)
@@ -60,9 +63,27 @@ const vm = new Vue({
       if (window.localStorage.carrinho) {
         this.carrinho = JSON.parse(window.localStorage.carrinho)
       }
+    },
+    alerta(mensagem) {
+      this.mensagemAlerta = mensagem;
+      this.alertaAtivo = true;
+      setTimeout(() => {
+        this.alertaAtivo = false
+      }, 1500)
+    },
+    router() {
+      const hash = document.location.hash
+      if (hash) {
+        this.fetchProduto(hash.replace("#", ""))
+      }
     }
   },
   watch: {
+    produto() {
+      document.title = this.produto.nome || "Techno"
+      const hash = this.produto.id || ""
+      history.pushState(null, null, `#${hash}`)
+    },
     carrinho() {
       window.localStorage.carrinho = JSON.stringify(this.carrinho)
     }
@@ -70,5 +91,6 @@ const vm = new Vue({
   created() {
     this.fetchProdutos()
     this.checarLocalStorage()
+    this.router()
   }
 })
