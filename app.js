9aa4c79f119d0b69de8b61e69b88fe0e61f6a3ff
helderavila <helderavila2016@gmail.com>
2020-01-20 10:45:33 -0300

    cart style

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const vm = new Vue({
     produtos: [],
     produto: false,
     carrinho: [],
+    carrinhoAtivo: false,
     mensagemAlerta: "Item adicionado ao carrinho",
     alertaAtivo: false
   },
@@ -48,6 +49,11 @@ const vm = new Vue({
     fecharModal({ target, currentTarget }) {
       if (target === currentTarget) {
         this.produto = false
+      }
+    },
+    clickForaCarrinho({ target, currentTarget }) {
+      if (target === currentTarget) {
+        this.carrinhoAtivo = false
       }
     },
     adicionarItem() {
